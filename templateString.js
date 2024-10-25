23bb8fb7f600ae035d1a07605885d82def0ede5b
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-25 15:21:13 +0600

    template string added

---
+++ templateString.js
@@ -0,0 +1,11 @@
+const numbers = [2, 1, 21, 42,]
+
+const students = {
+    name: "Masud Sarder",
+    age: 24,
+    propiton: ["web Developer", "Thingker"]
+};
+
+
+const about = `This is my Name ${students.name} age of ${students.age} has number ${numbers[1]} of he is good man`;
+console.log(about);
